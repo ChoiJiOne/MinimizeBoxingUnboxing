65430ac874488370f4c785597b1b191c073eb786
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-04-05 16:34:20 +0900

    프로젝트를 삭제하는 배치 스크립트 추가

---
+++ RemoveCSharpProject.bat
@@ -0,0 +1,10 @@
+@echo off
+
+SET SCRIPT_PATH=%~dp0Script\cli.py
+SET SCRIPT_COMMAND=remove-csharp-project
+SET ROOT_PATH=%~dp0
+SET SOLUTION_NAME=MinimizeBoxingUnboxing
+SET PROJECT_NAME=%1
+SET LOG_PATH=%~dp0Log
+
+python %SCRIPT_PATH% %SCRIPT_COMMAND% --root-path %ROOT_PATH% --solution-name %SOLUTION_NAME% --project-name %PROJECT_NAME% --log-path %LOG_PATH%
