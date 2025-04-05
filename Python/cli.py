db4a4efb5a1f5ecd15e34f45df49bbaf8c0247c8
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-04-05 16:15:53 +0900

    솔루션에 프로젝트를 추가 및 제거하는 기능 추가

--- Python/cli.py
+++ Python/cli.py
@@ -1,10 +1,10 @@
 import click
 import traceback
-import subprocess
+from pathlib import Path
 
+from command_utils import run_command
 from config import ProjectConfig
 from logger import Logger
-from command_utils import run_command
 
 @click.group()
 def cli():
@@ -15,14 +15,25 @@ def cli():
 @click.option("--solution-name", required=True)
 @click.option("--project-name", required=True)
 @click.option("--log-path", required=True)
-def create_csharp_project(**kwargs):
+def add_csharp_project(**kwargs):
     try:
         config = ProjectConfig(**kwargs)
         logger = Logger(config.log_path)
 
+        project_path = Path(config.root_path) / f"{config.project_name}"
+        if project_path.exists():
+            raise Exception(f"Already exist '{config.project_name}' project.")
+        
+        solution_path = Path(config.root_path) / f"{config.solution_name}.sln"
+        if not solution_path.exists():
+            raise Exception(f"Cannot found Visual Studio solution : '{config.solution_name}'")
+        
         command = f"dotnet new console --language \"C#\" --name {config.project_name}"
         run_command(logger, command)
-        
+
+        command = f"dotnet sln add {config.project_name}"
+        run_command(logger, command)
+
     except FileNotFoundError as e:
         click.echo(f"[ERROR] Invalid log path: {e}")
         traceback.print_exc()
@@ -32,6 +43,37 @@ def create_csharp_project(**kwargs):
     except Exception as e:
         click.echo(f"[ERROR] Unexpected error occurred: {e}")
         traceback.print_exc()
-    
+
+@cli.command()
+@click.option("--root-path", required=True)
+@click.option("--solution-name", required=True)
+@click.option("--project-name", required=True)
+@click.option("--log-path", required=True)
+def remove_csharp_project(**kwargs):
+    try:
+        config = ProjectConfig(**kwargs)
+        logger = Logger(config.log_path)
+
+        project_path = Path(config.root_path) / f"{config.project_name}"
+        if not project_path.exists():
+            raise Exception(f"Cannot found '{config.project_name}' project.")
+        
+        solution_path = Path(config.root_path) / f"{config.solution_name}.sln"
+        if not solution_path.exists():
+            raise Exception(f"Cannot found Visual Studio solution : '{config.solution_name}'")
+        
+        command = f"dotnet sln remove {config.project_name}"
+        run_command(logger, command)
+
+    except FileNotFoundError as e:
+        click.echo(f"[ERROR] Invalid log path: {e}")
+        traceback.print_exc()
+    except NotADirectoryError as e:
+        click.echo(f"[ERROR] Log path is not a directory: {e}")
+        traceback.print_exc()
+    except Exception as e:
+        click.echo(f"[ERROR] Unexpected error occurred: {e}")
+        traceback.print_exc()
+
 if __name__ == "__main__":
     cli()
