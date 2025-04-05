5941abe6b3fe03d7ee9e4305e283f764506785fb
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-04-05 16:00:52 +0900

    로그 파일 생성 부분 수정

--- Python/logger.py
+++ Python/logger.py
@@ -2,15 +2,14 @@ from datetime import datetime
 from pathlib import Path
 import logging
 
+from file_utils import is_directory
+
 class Logger:
     def __init__(self, log_path: str):
         self.log_file_name = self.generate_log_file_name()
         self.log_format = "[%(asctime)s][%(levelname)s] %(message)s"
         self.date_format = "%Y-%m-%d %H:%M:%S"
-
-        path = self.generate_log_file_path(log_path)
-        path = path.joinpath(self.log_file_name)
-        self.log_file_path = str(path)
+        self.log_file_path = self.generate_log_file_path(log_path, self.log_file_name)
 
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.INFO)
@@ -29,15 +28,14 @@ class Logger:
         time_stamp = now.strftime("%Y_%m_%d__%H_%M_%S")
         return f"{time_stamp}_log.txt"
     
-    def generate_log_file_path(self, log_path: str):
-        path = Path(log_path)
-        if not path.exists():
-            raise FileNotFoundError(f"The path '{log_path}' does not exist.")
-
-        if not path.is_dir():
-            raise NotADirectoryError(f"The path '{log_path}' is not a directory")
+    def generate_log_file_path(self, log_path: str, log_file_name: str):
+        if not is_directory(log_path):
+            raise NotADirectoryError(f"The path '{log_path}' does not exist. or not a directory")
         
-        return path
+        log_file_path = Path(log_path)
+        log_file_path = log_file_path.joinpath(log_file_name)
+        
+        return str(log_file_path)
     
     def info(self, message):
         self.logger.info(message)
