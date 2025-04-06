4f344cc5a7ccf31896a08288ad97719dc37b658f
CHOIJIONE\choij <choijione02@gmail.com>
2025-04-07 07:59:34 +0900

    구조체 복사 예제 추가

---
+++ 08.StructCopy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public struct Person
+{
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> attendees = new List<Person>();
+
+        Person person = new Person { Name = "Old Name" };
+        attendees.Add(person);
+
+        Person firstPerson = attendees[0];
+        firstPerson.Name = "New Name";
+
+        Console.WriteLine(attendees[0].ToString());
+    }
+}
+
