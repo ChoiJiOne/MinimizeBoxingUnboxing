3f806c2e0421996c9ebd03b2878fcce738a016bf
CHOIJIONE\choij <choijione02@gmail.com>
2025-04-07 07:41:13 +0900

    $를 이용한 문자열 보간 예제 추가

---
+++ 06.StringInterpolation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstNumber = 100;
+        int secondNumber = 200;
+        int thirdNumber = 300;
+
+        string message = $"A few numbers: {firstNumber}, {secondNumber}, {thirdNumber}";
+        Console.WriteLine(message);
+    }
+}
