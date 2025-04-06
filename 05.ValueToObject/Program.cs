db89afff1122c7fa86abc39c2bba0bfd494e2960
CHOIJIONE\choij <choijione02@gmail.com>
2025-04-07 07:41:01 +0900

    System.Object 타입을 인자로 받는 메서드 예제 추가

---
+++ 05.ValueToObject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+struct Position
+{
+    public float X;
+    public float Y;
+    public float Z;
+}
+
+class Program
+{
+    static void ShowObject(object obj)
+    {
+        Console.WriteLine($"{obj}");
+    }
+
+    static void Main(string[] args)
+    {
+        int number = 100;
+        ShowObject(number);
+
+        Position position;
+        position.X = 1.0f;
+        position.Y = 2.0f;
+        position.Z = 3.0f;
+        ShowObject(position);
+    }
+}
