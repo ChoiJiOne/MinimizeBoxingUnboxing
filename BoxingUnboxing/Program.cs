8965a4e942fe55394093a9e8315578bb6edb073b
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-04-06 13:40:57 +0900

    간단한 박싱/언박싱 예제 프로젝트 추가

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 100;
+        object boxingNumber = (object)(number);
+        int unboxingNumber = (int)(boxingNumber);
+
+        Console.WriteLine($"Number: {number}");
+        Console.WriteLine($"Boxing number: {boxingNumber}");
+        Console.WriteLine($"Unboxing object: {unboxingNumber}");
+    }
+}
