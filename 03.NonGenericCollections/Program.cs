cda2ec75e25693e6f651dc599d92051077ecab50
CHOIJIONE\choij <choijione02@gmail.com>
2025-04-07 07:40:34 +0900

    제너릭이 아닌 컬렉션을 사용하는 예제 추가

---
+++ 03.NonGenericCollections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue queue = new Queue();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        queue.Enqueue(5);
+
+        while (queue.Count > 0)
+        {
+            Console.WriteLine($"{queue.Dequeue()}");
+        }
+    }
+}
