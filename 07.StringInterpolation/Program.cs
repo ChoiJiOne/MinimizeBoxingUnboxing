dd565802f44b6079019f3f053436bc2179fd035c
CHOIJIONE\choij <choijione02@gmail.com>
2025-04-07 07:41:33 +0900

    Format을 이용한 포멧팅 문자열 예제 추가

---
+++ 07.StringInterpolation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstNumber = 100;
+        int secondNumber = 200;
+        int thirdNumber = 300;
+
+        string message = String.Format("A few numbers: {0}, {1}, {2}", firstNumber, secondNumber, thirdNumber);
+        Console.WriteLine(message);
+    }
+}
