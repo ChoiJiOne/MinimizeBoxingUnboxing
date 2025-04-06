029c50257a14a1a3f1e661a5bf1d0c0d46aaa9b8
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-04-06 14:14:50 +0900

    값 타입 구조체의 박싱/언박싱 예제 프로젝트 추가

---
+++ 02.BoxingUnboxing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+struct Position
+{
+    public float X;
+    public float Y;
+    public float Z;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Position position;
+        position.X = 1.0f;
+        position.Y = 2.0f;
+        position.Z = 3.0f;
+
+        object boxingPosition = (object)(position);
+        Position unboxingPosition = (Position)(boxingPosition);
+
+        Console.WriteLine($"Position: ({unboxingPosition.X}, {unboxingPosition.Y}, {unboxingPosition.Z})");
+    }
+}
