93665cfdb19616b020b449b89639e56b989829f2
CHOIJIONE\choij <choijione02@gmail.com>
2025-04-08 07:02:37 +0900

    박싱 벤치마크를 수행하는 샘플 프로젝트 추가

---
+++ 09.BoxingBenchmark/Program.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+[MemoryDiagnoser]
+[ShortRunJob]
+public class BoxingBenchmarks
+{
+    private int _iterationCount = 1000000;
+
+    [Benchmark]
+    public int WithBoxing()
+    {
+        object boxed;
+        int sum = 0;
+        for (int i = 0; i < _iterationCount; i++)
+        {
+            boxed = i;
+            sum += (int)boxed;
+        }
+        return sum;
+    }
+
+    [Benchmark]
+    public int WithoutBoxing()
+    {
+        int sum = 0;
+        for (int i = 0; i < _iterationCount; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<BoxingBenchmarks>();
+    }
+}
